dac94444019d95398bc59765bc68f59aa2fd3567
Emmett Butler <emmett.butler321@gmail.com>
2014-07-13 01:02:01 -0400

    move linux branch into master

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm ~/.bashrc
+rm ~/.vimrc
+
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/vimrc ~/.vimrc
+
+mkdir ~/.vim
+mkdir ~/.vim/colors
+
+cp ~/dotfiles/molokai.vim ~/.vim/colors
+cp ~/dotfiles/candycode.vim ~/.vim/colors
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
